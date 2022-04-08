8a9eb9d02bad873e98b30b903a6f93c4db763c5f
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-04-08 23:56:06 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s=62+65+"Alok+85+65";
+        System.out.println(s);
+	// write your code here
+    }
+}
